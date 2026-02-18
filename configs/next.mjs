82f8a876b53b15c7f8e425624960447a1634a5cb
Martin Buberl <hi@martinbuberl.com>
2026-02-18 14:03:51 +0100

    Add Tailwind rules to next config

--- configs/next.mjs
+++ configs/next.mjs
@@ -1,3 +1,5 @@
+import tailwindcssPlugin from 'eslint-plugin-tailwindcss';
+
 const next = [
   {
     name: '@100xventures/eslint-config/next',
@@ -6,6 +8,16 @@ const next = [
       'src/components/ui/', // Ignore shadcn/ui components
       'src/hooks/use-mobile.ts', // Ignore shadcn/ui use-mobile hook
     ],
+  },
+  {
+    files: ['**/*.{js,jsx,mjs,ts,tsx}'],
+    plugins: {
+      tailwindcss: tailwindcssPlugin,
+    },
+    rules: {
+      'tailwindcss/classnames-order': 'error', // Enforces classnames order
+      'tailwindcss/enforces-shorthand': 'error', // Enforces shorthand syntax
+    },
   },
 ];
 
